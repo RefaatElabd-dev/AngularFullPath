b085b31b9a06ae9d08972a6c2289e076f87644c4
Refaat <RefaatElabd7@gmail.com>
2022-10-27 01:51:47 +0200

    PreLoading All Modules

--- src/app/app-routing-module.ts
+++ src/app/app-routing-module.ts
@@ -1,18 +1,31 @@
 import { NgModule } from "@angular/core";
-import { RouterModule, Routes } from "@angular/router";
+import { PreloadAllModules, RouterModule, Routes } from "@angular/router";
 
 import { AuthComponent } from "./auth/auth.component";
 
 
 const appRoutes: Routes = [
-    { path: '', redirectTo: '/auth', pathMatch: 'full' },
-    { path: 'recipes', loadChildren: () => import('./recipes/recipes.module').then(m => m.RecipesModule)},
-    { path: 'shopping-list', loadChildren: () => import('./shopping-list/shopping-list.module').then(m => m.shoppingListModule)},
-    { path: 'auth', loadChildren: () => import('./auth/auth.module').then(m => m.AuthModule)},
-]
+  { path: "", redirectTo: "/auth", pathMatch: "full" },
+  {
+    path: "recipes",
+    loadChildren: () =>
+      import("./recipes/recipes.module").then((m) => m.RecipesModule),
+  },
+  {
+    path: "shopping-list",
+    loadChildren: () =>
+      import("./shopping-list/shopping-list.module").then(
+        (m) => m.shoppingListModule
+      ),
+  },
+  {
+    path: "auth",
+    loadChildren: () => import("./auth/auth.module").then((m) => m.AuthModule),
+  },
+];
 
 @NgModule({
-    imports: [RouterModule.forRoot(appRoutes)],
+    imports: [RouterModule.forRoot(appRoutes, { preloadingStrategy: PreloadAllModules})],
     exports: [RouterModule]
 })
 export class AppRouting {
