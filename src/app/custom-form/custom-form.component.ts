5c719c367ad476583a174690f00f06081d346653
Refaat Elabd <RefaatElabd7@gmail.com>
2024-01-01 23:56:52 +0200

    update references

--- src/app/custom-form/custom-form.component.ts
+++ src/app/custom-form/custom-form.component.ts
@@ -5,7 +5,7 @@ import { Component, OnInit } from "@angular/core";
     templateUrl: './custom-form.component.html',
   })
   export class CustomFormComponent implements OnInit {
-    phone2: string = '011519588'
+
     phone: string = '011519588'
     ngOnInit(): void {
         
