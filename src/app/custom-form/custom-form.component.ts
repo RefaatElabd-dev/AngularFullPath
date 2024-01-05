a8d32356480cc2f64c69c33f2b2c2523493d8f52
Refaat Elabd <RefaatElabd7@gmail.com>
2024-01-05 22:13:07 +0200

    add for controls navitem

--- src/app/custom-form/custom-form.component.ts
+++ src/app/custom-form/custom-form.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from "@angular/core";
+import { CheckTypeEnum } from "ngx-custom-comps";
 
 @Component({
     selector: 'custom-form',
@@ -7,7 +8,9 @@ import { Component, OnInit } from "@angular/core";
   export class CustomFormComponent implements OnInit {
 
     phone: string = '011519588';
-    isChecked: boolean = true;
+    isChecked: boolean = false;
+    magicCheckType: CheckTypeEnum = CheckTypeEnum.Magic
+    toggleCheckType: CheckTypeEnum = CheckTypeEnum.Toggle
     ngOnInit(): void {
         
     }
